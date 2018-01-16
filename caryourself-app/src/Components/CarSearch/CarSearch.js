b42ab9884aaf7835c4b132e73ed5ed05f7ba2bb9
jacekkowalczyk91 <kowalczykjacek91@gmail.com>
2018-01-16 15:44:04 +0100

    first steps

---
+++ caryourself-app/src/Components/CarSearch/CarSearch.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {
+    InputGroup,
+    FormControl,
+    Button
+} from 'react-bootstrap'
+
+class CarSearch extends React.Component {
+    render() {
+        return (
+            <form>
+                <InputGroup>
+                    <FormControl
+                    />
+                </InputGroup>
+            </form>
+        )
+    }
+}
+
+export default CarSearch
